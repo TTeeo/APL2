fdc495463ce8e56e6b2803ffe7002a8aaf036e91
Dabrensch <116101463+Dabrensch@users.noreply.github.com>
2024-10-20 19:58:06 -0300

    se sacaron comentarios redundantes

--- ejercicio5/servidor/src/servidor.cpp
+++ ejercicio5/servidor/src/servidor.cpp
@@ -70,8 +70,6 @@ void Servidor::manejadorCliente(int socketCliente) {
       TransmisionMensajes::recibirMensaje(socketCliente, &opcionElegida,
                                           sizeof(int), 0);
     } catch (const exception &) {
-      cout << "Cliente " << mensajeCliente.nickname << " se ha desconectado."
-           << endl;
       return;
     }
     if (clienteAcerto(opcionElegida,
@@ -127,7 +125,6 @@ int Servidor::aceptarConexion() {
 
   bool salaLlena = false;
   TransmisionMensajes::enviarMensaje(socketCliente, &salaLlena, sizeof(bool));
-  cout << "Conexion aceptada" << endl;
   return socketCliente;
 }
 
